c964bbd56fd41542ccb00b052e11fd60e7d719ca
Bhavesh suthar <bhaveshsutharkulariya123456@gmail.com>
2025-12-19 18:00:42 +0530

    new

--- Numpy/Numpy.5.py
+++ Numpy/Numpy.5.py
@@ -1,2 +1,21 @@
 """Handling Missing Value"""
 
+import numpy as np
+
+"""
+    np.isnan ->>> its detect missing value
+
+    np.nan_to_num ->> if you want to fill missing value from someone so we use np.nan_to_num
+
+    np.isinf() --> its detect infinte number
+"""
+
+arr = np.array([1,2,3,np.nan , 4 , np.nan] , dtype=float)
+
+print(np.isnan(arr))
+
+Clear = np.nan_to_num(arr , 100)
+print(Clear)
+"""we can't compare directly 
+    np.nan == np.nan
+"""
